146cc8ef1b847ca6803b5d0e6984a6ac1674cd02
ctenetliu <1505674928@qq.com>
2024-04-02 17:42:50 +0800

    feat: 添加用户管理模块，实现swagger鉴权 (closes #3)

--- modules/user_manage/views.py
+++ modules/user_manage/views.py
@@ -4,6 +4,7 @@ from bk_resource.viewsets import ResourceRoute, ResourceViewSet
 
 
 class UserManageViewSet(ResourceViewSet):
+    # TODO 添加权限控制
     resource_routes = [
         # 查询用户列表
         ResourceRoute("GET", resource.user_manage.get_users, endpoint="users_list"),
