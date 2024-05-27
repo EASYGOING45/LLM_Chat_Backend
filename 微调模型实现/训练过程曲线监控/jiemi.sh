fabab92608438a87bffdb38c383bbdc2b1caa5cd
EASYGOING45 <1505674928@qq.com>
2024-05-27 16:56:35 +0800

    feat: 模型监控，LangChain启动Bash脚本 #13

---
+++ 微调模型实现/训练过程曲线监控/jiemi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source activate py310_chat
+python /root/ChatGLM3/resources/jiemi.py
---
+++ 微调模型实现/训练过程曲线监控/langchain.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source activate langchain
+cd /root/Langchain-Chatchat
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
